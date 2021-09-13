c29bb7cbf70a3debe0cbaeb384f5960abc0f18bc
Amadeusz Sepko <a.sepko@gmail.com>
2021-09-12 22:51:51 -0400

    Redux Adding Favorites

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { FlatList } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = (state) => {
 	return {
@@ -20,7 +21,16 @@ class Directory extends Component {
 		const renderDirectoryItem = ({ item }) => {
 			return <Tile title={item.name} caption={item.description} featured onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })} imageSrc={{ uri: baseUrl + item.image }} />;
 		};
-
+		if (this.props.campsites.isLoading) {
+			return <Loading />;
+		}
+		if (this.props.campsites.errMess) {
+			return (
+				<View>
+					<Text>{this.props.campsites.errMess}</Text>
+				</View>
+			);
+		}
 		return <FlatList data={this.props.campsites.campsites} renderItem={renderDirectoryItem} keyExtractor={(item) => item.id.toString()} />;
 	}
 }
