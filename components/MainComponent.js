2217bff3593471969b723228dac91f1e98fb55d3
Amadeusz Sepko <a.sepko@gmail.com>
2021-09-13 14:46:18 -0400

    Modal

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,7 @@ import CampsiteInfo from './CampsiteInfoComponent';
 import Home from './HomeComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
+import Reservation from './ReservationComponent';
 import Constants from 'expo-constants';
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
@@ -99,6 +100,24 @@ const ContactNavigator = createStackNavigator(
 	}
 );
 
+const ReservationNavigator = createStackNavigator(
+	{
+		Reservation: { screen: Reservation },
+	},
+	{
+		defaultNavigationOptions: ({ navigation }) => ({
+			headerStyle: {
+				backgroundColor: '#5637DD',
+			},
+			headerTintColor: '#fff',
+			headerTitleStyle: {
+				color: '#fff',
+			},
+			headerLeft: <Icon name="tree" type="font-awesome" iconStyle={styles.stackIcon} onPress={() => navigation.toggleDrawer()} />,
+		}),
+	}
+);
+
 const CustomDrawerContentComponent = (props) => (
 	<ScrollView>
 		<SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
@@ -127,6 +146,13 @@ const MainNavigator = createDrawerNavigator(
 			screen: DirectoryNavigator,
 			navigationOptions: {
 				drawerIcon: ({ tintColor }) => <Icon name="list" type="font-awesome" size={24} color={tintColor} />,
+			},
+		},
+		Reservation: {
+			screen: ReservationNavigator,
+			navigationOptions: {
+				drawerLabel: 'Reserve Campsite',
+				drawerIcon: ({ tintColor }) => <Icon name="tree" type="font-awesome" size={24} color={tintColor} />,
 			},
 		},
 		About: {
