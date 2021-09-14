b403c2c7a2b79aec849ec3e9fb6b8fdf288516c7
Amadeusz Sepko <a.sepko@gmail.com>
2021-09-14 19:00:25 -0400

    Gestures 1 and 2

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -44,9 +44,42 @@ function RenderComments({ comments }) {
 function RenderCampsite(props) {
 	const { campsite } = props;
 
+	const view = React.createRef();
+
+	const recognizeDrag = ({ dx }) => (dx < -200 ? true : false);
+
+	const panResponder = PanResponder.create({
+		onStartShouldSetPanResponder: () => true,
+		onPanResponderGrant: () => {
+			view.current.rubberBand(1000).then((endState) => console.log(endState.finished ? 'finished' : 'canceled'));
+		},
+		onPanResponderEnd: (e, gestureState) => {
+			console.log('pan responder end', gestureState);
+			if (recognizeDrag(gestureState)) {
+				Alert.alert(
+					'Add Favorite',
+					'Are you sure you wish to add ' + campsite.name + ' to favorites?',
+					[
+						{
+							text: 'Cancel',
+							style: 'cancel',
+							onPress: () => console.log('Cancel pressed'),
+						},
+						{
+							text: 'OK',
+							onPress: () => (props.favorite ? console.log('Already set as favorite') : props.markFavorite()),
+						},
+					],
+					{ cancelable: false }
+				);
+			}
+			return true;
+		},
+	});
+
 	if (campsite) {
 		return (
-			<Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+			<Animatable.View animation="fadeInDown" duration={2000} delay={1000} ref={view} {...panResponder.panHandlers}>
 				<Card featuredTitle={campsite.name} image={{ uri: baseUrl + campsite.image }}>
 					<Text style={{ margin: 10 }}>{campsite.description}</Text>
 					<View style={styles.cardRow}>
