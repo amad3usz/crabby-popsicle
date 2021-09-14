87d45aa44d03a3cc3ecd5d2a5461a1a9e92d69da
Amadeusz Sepko <a.sepko@gmail.com>
2021-09-13 21:40:33 -0400

    Swipe and Delete Favorites

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -4,6 +4,7 @@ import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = (state) => {
 	return {
@@ -19,7 +20,11 @@ class Directory extends Component {
 	render() {
 		const { navigate } = this.props.navigation;
 		const renderDirectoryItem = ({ item }) => {
-			return <Tile title={item.name} caption={item.description} featured onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })} imageSrc={{ uri: baseUrl + item.image }} />;
+			return (
+				<Animatable.View animation="fadeInRightBig" duration={2000}>
+					<Tile title={item.name} caption={item.description} featured onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })} imageSrc={{ uri: baseUrl + item.image }} />
+				</Animatable.View>
+			);
 		};
 		if (this.props.campsites.isLoading) {
 			return <Loading />;
--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -158,6 +158,11 @@ export const addFavorite = (campsiteId) => ({
 	payload: campsiteId,
 });
 
+export const deleteFavorite = (campsiteId) => ({
+	type: ActionTypes.DELETE_FAVORITE,
+	payload: campsiteId,
+});
+
 export const postComment = (campsiteId, rating, author, text) => (dispatch) => {
 	const newComment = {
 		campsiteId,
