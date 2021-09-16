3f23829c7b0c2e5aae218eb0d8f6d69c55422318
Amadeusz Sepko <a.sepko@gmail.com>
2021-09-15 23:22:26 -0400

    Social Sharing

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet, Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -80,6 +80,19 @@ function RenderCampsite(props) {
 		},
 	});
 
+	const shareCampsite = (title, message, url) => {
+		Share.share(
+			{
+				title: title,
+				message: `${title}: ${message} ${url}`,
+				url: url,
+			},
+			{
+				dialogTitle: 'Share ' + title,
+			}
+		);
+	};
+
 	if (campsite) {
 		return (
 			<Animatable.View animation="fadeInDown" duration={2000} delay={1000} ref={view} {...panResponder.panHandlers}>
@@ -95,6 +108,7 @@ function RenderCampsite(props) {
 							onPress={() => (props.favorite ? console.log('Already set as a favorite') : props.markFavorite())}
 						/>
 						<Icon name="pencil" type="font-awesome" color="#5637DD" raised reverse onPress={() => props.onShowModal()} />
+						<Icon name={'share'} type="font-awesome" color="#5637DD" raised reverse onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)} />
 					</View>
 				</Card>
 			</Animatable.View>
